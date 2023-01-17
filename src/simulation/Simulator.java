6d59f33c05ef5bf659eceec0092197ffa72f3d2a
Dasith U Edirisinghe <udasith@gmail.com>
2023-01-17 20:30:10 +0530

    simulator updated

--- src/simulation/Simulator.java
+++ src/simulation/Simulator.java
@@ -5,7 +5,7 @@ import models.BoardingArea;
 import java.util.Random;
 
 public abstract class Simulator implements Runnable {
-    private final int TIME_INTERVAL;
+    private final float TIME_INTERVAL;
     protected final BoardingArea boardingArea;
     private final Random random;
     public Simulator(int TIME_INTERVAL, BoardingArea boardingArea) {
@@ -30,10 +30,10 @@ public abstract class Simulator implements Runnable {
         }
     }
 
-    private int getRandomTimeInterval() {
-//        float lambda = 1 / TIME_INTERVAL;
-//        return Math.round(Math.log(1 - random.nextFloat()) / (-lambda));
-        return TIME_INTERVAL;
+    private long getRandomTimeInterval() {
+        float lambda = 1 / TIME_INTERVAL;
+        return Math.round(Math.log(1 - random.nextFloat()) / (-lambda));
+//        return TIME_INTERVAL;
     }
     protected abstract Runnable getRunnable(int id, BoardingArea boardingArea);
 }
